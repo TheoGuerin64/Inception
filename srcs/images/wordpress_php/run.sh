3f9d2c955b0e75905469a9ece6ff08e0e3a7f2f9
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2024-04-16 15:11:11 +0200

    Maintain and fix wordpress bug (--path not working)

--- srcs/images/wordpress_php/run.sh
+++ srcs/images/wordpress_php/run.sh
@@ -4,10 +4,11 @@ if [ ! -f "/home/wordpress/index.php" ]; then
 		sleep 1
 	done
 
-	wp core download --path="/home/wordpress" 
-	wp config create --path="/home/wordpress" --dbname=wordpress --dbuser=$MARIADB_ADMIN_NAME --dbpass=$MARIADB_ADMIN_PASS --dbhost=mariadb
-	wp core install --path="/home/wordpress" --url=tguerin.42.fr --title=Inception --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=$WORDPRESS_ADMIN_MAIL --skip-email
-	wp user create --path="/home/wordpress" --user_pass=$WORDPRESS_USER_PASS $WORDPRESS_USER_NAME $WORDPRESS_USER_MAIL
+	cd /home/wordpress
+	wp core download 
+	wp config create --dbname=wordpress --dbuser=$MARIADB_ADMIN_NAME --dbpass=$MARIADB_ADMIN_PASS --dbhost=mariadb
+	wp core install --url=localhost --title=Inception --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=$WORDPRESS_ADMIN_MAIL --skip-email
+	wp user create --user_pass=$WORDPRESS_USER_PASS $WORDPRESS_USER_NAME $WORDPRESS_USER_MAIL
 fi
 
 exec php-fpm8 -F
